d5aa5d3b2ca76d893038e2ade3c5594ceb332359
irmoralesb <irmoralesb@hotmail.com>
2019-11-27 14:25:30 -0600

    Replacing template's files with the project's ones

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AuthorQuiz.css';
+import './bootstrap.min.css';
+
+class AuthorQuiz extends React.Component{
+  render() {
+    return(
+      <div>Author Quiz</div>
+    )}
+}
+
+export default AuthorQuiz;
